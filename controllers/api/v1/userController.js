5f717f48b018a0e7a9b96a9d81ff96225b1cd7a5
zghr <r0878962@student.thomasmore.be>
2024-03-07 15:44:21 +0100

    Chore: add phonenumber to user

--- controllers/api/v1/userController.js
+++ controllers/api/v1/userController.js
@@ -5,10 +5,10 @@ const {User} = require('../../../models/api/v1/User');
 // create new user
 const createUser = async (req, res) => {
     try {
-        const { first_name, last_name, email, password, role, jeugdbeweging } = req.body;
+        const { first_name, last_name, email, phone, password, role, jeugdbeweging } = req.body;
 
         // create new user with hashed password
-        const newUser = new User({ first_name, last_name, email, password, role, jeugdbeweging });
+        const newUser = new User({ first_name, last_name, email, phone, password, role, jeugdbeweging });
 
         await newUser.save();
 
--- models/api/v1/User.js
+++ models/api/v1/User.js
@@ -15,6 +15,10 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+    phone:{
+        type: String,
+        required: true
+    },
     password: {
         type: String,
         required: true
