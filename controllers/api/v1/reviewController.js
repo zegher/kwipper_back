846de82cac0d5e06438f0af15afc1516b1877626
zghr <r0878962@student.thomasmore.be>
2024-03-21 11:39:22 +0100

    Feat: get & post user review

--- controllers/api/v1/reviewController.js
+++ controllers/api/v1/reviewController.js
@@ -12,8 +12,49 @@ const getAllReviews = async (req, res) => {
     }
 };
 
+//get review based on id
+// const getReviewsById = async (req, res) => {
+//     try {
+//         const { id } = req.params;
+
+//         if(!id) {
+//             return res.status(400).json({ message: 'User id is required' });
+//         }
+//         const review = await Review.findById(id);
+
+//         if(!review) {
+//             return res.status(404).json({ message: 'Review not found' });
+//         }
+//         res.status(200).json({ data: { review } });
+//     }
+//     catch (error) {
+//         console.error('Error getting review by id:', error);
+//         res.status(500).json({ message: 'Internal Server Error' });
+//     }
+// };
+
+//post review
+const postReview = async (req, res) => {
+    try {
+        const { posted_by, user_reviewed, rating, description } = req.body;
+
+        // if(!review || !rating || !user || !product) {
+        //     return res.status(400).json({ message: 'Review, rating, user and product are required' });
+        // }
+
+        const newReview = new Review({ posted_by, user_reviewed, rating, description });
+
+        await newReview.save();
+
+        res.status(201).json({ message: 'Review created successfully', data: { review: newReview }});
+    } catch (error) {
+        console.error('Error posting review:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
 // Export the createUser function
 module.exports = {
-    getAllReviews
+    getAllReviews, postReview
 };
 
--- routes/api/v1/review.js
+++ routes/api/v1/review.js
@@ -5,4 +5,7 @@ const reviewController = require('../../../controllers/api/v1/reviewController')
 //get all reviews
 router.get('/', reviewController.getAllReviews);
 
+//post review
+router.post('/', reviewController.postReview);
+
 module.exports = router
