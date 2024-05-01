d87d4153bc170fa249add57b93cf9c8e9190ccb8
zghr <r0878962@student.thomasmore.be>
2024-05-01 12:19:53 +0200

    Feat: add createAssortment for the new structure

--- controllers/api/v1/twoAssortmentcontroller.js
+++ controllers/api/v1/twoAssortmentcontroller.js
@@ -12,7 +12,27 @@ const getAss2 = async (req, res) => {
     }
 }
 
+//create new assortment based on two model
+const createAssortment2 = async (req, res) => {
+    try {
+        const { item, price, waarborg, available_from, available_until, art_desc, art_category, condition, size, brand, complete_set, free, premium, posted_by, location } = req.body;
+
+        // if(!name || !description || !price || !quantity || !image || !posted_by) {
+        //     return res.status(400).json({ message: 'All fields are required' });
+        // }
+
+        const newAssortment = await Two.create({
+            item, price, waarborg, available_from, available_until, art_desc, art_category, condition, size, brand, complete_set, free, premium, posted_by, location 
+        });
+
+        res.status(201).json({ data: { assortment: newAssortment } });
+    } catch (error) {
+        console.error('Error creating new assortment:', error);
+        res.status(500).json({ message: 'Internal Server Error - createAssortment2' });
+    }
+}
+
 // Export the createUser function
 module.exports = {
-    getAss2
+    getAss2, createAssortment2
 };
