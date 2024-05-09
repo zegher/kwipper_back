508a4166d707b270b632cc15485a6f9f5327da78
zghr <r0878962@student.thomasmore.be>
2024-05-09 17:16:49 +0200

    Feat: get tokens

--- controllers/api/v1/tokenController.js
+++ controllers/api/v1/tokenController.js
@@ -1,6 +1,18 @@
 const {Token} = require('../../../models/api/v1/Token');
 
+//get tokens
+const getTokens = async (req, res) => {
+    try {
+        const tokens = await Token.find();
+
+        res.status(200).json({ data: { tokens } });
+    } catch (error) {
+        console.error('Error getting all tokens:', error);
+        res.status(500).json({ message: 'Internal Server Error - getAllTokens' });
+    }
+};
+
 // Export the createUser function
 module.exports = {
-
+    getTokens,
 };
--- routes/api/v1/token.js
+++ routes/api/v1/token.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 const tokenController = require('../../../controllers/api/v1/tokenController')
 
+router.get('/', tokenController.getTokens)
 
 module.exports = router
