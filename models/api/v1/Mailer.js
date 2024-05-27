f7c685e7f4db06aeaf13bd9e3da1229cca349a25
zghr <r0878962@student.thomasmore.be>
2024-05-27 14:36:24 +0200

    Chore: tweaking mail visuals

--- models/api/v1/Mailer.js
+++ models/api/v1/Mailer.js
@@ -18,7 +18,7 @@ const mailer = {
                 to: email,
                 subject: 'Verifieer jouw email | Kwipper',
                 text: `Hey! Je moet nog één ding doen voor je kan beginnen broswen! Klik op de volgende link op jouw e-mail te verifieren: ${url}?token=${token} Hopelijk tot snel! Groetjes het Kwipper-team!`,
-                html: `<h2>Hey!</h2> <br> <br> <p>Je moet nog één ding doen voor je kan beginnen browsen! <br> Klik op de volgende link op jouw e-mail te verifieren: <br> <br>  <a  href="${url}?token=${token}" style="background-color: #2B5740; padding: 6px; border: 12px solid;">Verify Email</a> <br> <br> Hopelijk tot snel! <br> Groetjes het Kwipper-team! </p>`
+                html: `<h2>Hey!</h2> <br> <br> <p>Je moet nog één ding doen voor je kan beginnen browsen! <br> Klik op de volgende link op jouw e-mail te verifieren: <br> <br>  <a href="${url}?token=${token}" style="background-color: #2B5740; padding: 6px; border: 12px solid #000;">Verify Email</a> <br> <br> Hopelijk tot snel! <br> Groetjes het Kwipper-team! </p>`
             });
         }
     };
