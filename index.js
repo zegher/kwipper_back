fb86e121f776095b71fb43b9299cc40a06d57934
zghr <r0878962@student.thomasmore.be>
2024-05-28 16:24:34 +0200

    Chore: bugg fix within file upload2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const { image } = require('express-fileupload'); 
+const fileUpload = require('express-fileupload'); 
 
 // create express app
 const app = express();
