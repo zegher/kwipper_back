fb29bf67f8dd151bd590b9379346f1a4524fe2c7
zghr <r0878962@student.thomasmore.be>
2024-05-30 17:38:52 +0200

    Started on wishlist: added model

---
+++ models/api/v1/Wishlist.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { type } = require('os');
+const Schema = mongoose.Schema;
+
+// schema for shoppingCart
+const WishlistItem = new Schema({
+    amount: {
+        type: Number,
+        required: true
+    },
+    product_id: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    added_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = { WishlistItem };
