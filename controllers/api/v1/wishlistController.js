c1179ef7949293050fcd8197a2c925a133a82818
zghr <r0878962@student.thomasmore.be>
2024-05-31 11:36:57 +0200

    Chore: GET wishlist item & POST wishlist item (try out 1)

--- controllers/api/v1/wishlistController.js
+++ controllers/api/v1/wishlistController.js
@@ -11,7 +11,37 @@ const getWishlist = async (req, res) => {
     }
 };
 
+//post wishlist
+const addWishlistItem = async (req, res) => {
+    try {
+        const { product_id } = req.body;
+        const userId = req.userId;
+
+        if (!userId || !product_id) {
+            return res.status(400).json({ message: 'User ID and product ID are required' });
+        }
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        user.wishlist.push({ product_id });
+
+        await user.save();
+
+        res.status(200).json({ message: 'Wishlist item added successfully', data: { wishlist: user.wishlist } });
+    } catch (error) {
+        console.error('Error adding wishlist item:', error);
+        res.status(500).json({ message: 'Internal Server Error - addWishlistItem' });
+    }
+}
+
+//delete wishlist
+//update wishlist item
+
 
 module.exports = {
-    getWishlist,
+    getWishlist, addWishlistItem,
 };
--- routes/api/v1/wishlist.js
+++ routes/api/v1/wishlist.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const wishlistController = require('../../../controllers/api/v1/wishlistController.js');
 
 router.get('/', wishlistController.getWishlist);
+router.post('/', wishlistController.addWishlistItem);
 
 module.exports = router;
